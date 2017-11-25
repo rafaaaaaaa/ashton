c1b7b725a661c21e0528df674a8405727a0fc64e
rafaaaaaaa <33682680+rafaaaaaaa@users.noreply.github.com>
2017-11-25 14:40:20 +0100

    MainPageModel added

---
+++ ashton/Models/MainPageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ashton.Models
+{
+	public class MainPageModel
+	{
+		private List<Assignment> _assignments;
+
+		public MainPageModel(List<Assignment> assignments)
+		{
+			_assignments = assignments;
+		}
+
+		public List<Assignment> Assignments
+		{
+			get => _assignments;
+			set => _assignments = value;
+		}
+	}
+}
